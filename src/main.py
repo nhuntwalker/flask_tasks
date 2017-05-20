4fc9b20be9951e278874e0153cf0d7fb3f597e52
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-05-19 18:01:09 -0700

    Getting set up with a simple first view

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 """Main python file for the app."""
-from flask import Flask, request, response
+from flask import Flask
 
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home_list():
+    """View for listing out all of the tasks on deck."""
+    return "This is the home list page"
